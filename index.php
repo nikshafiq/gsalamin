29851632525a7278520621f2d1906f3793a9c561
KhairulAzry <mkhairulazry@Khairuls-MacBook-Air.local>
2018-03-05 23:41:18 +0800

    some minor changes

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]>      <html class="no-js" lang=""> <!<![endif]-->
 
-<html mlns="http://www.w3.org/1999/xhtml">
+<html>
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<LINK href="css/stylesheet.css" rel="stylesheet" type="text/css">
-	<title>GS al-Amin</title>
+
 </head>
 
 
 
 <body>
-	asd
+	test
 </body>
-
-
 
 </html>
